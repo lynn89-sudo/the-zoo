976b5644020d0cfc1337878c8ab4ac08c05dab38
M J <luke87joseph@gmail.com>
2025-07-03 17:43:55 -0400

    Working on Main page

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <div id="navbar">
-    <img id="flag" src="flag.png" alt="Hand holding out a flag which says Hack Club, and links to the Hack Club Website"/>
     <img id = "logo" src="logo.png" alt="Logo for The Zoo, a Hack Club You Ship We Ship program"/>
 </div>
 <div id="spacing"></div>
+<h2>You ship</h2>
+<h1>A SVELTE SITE</h1>
+<h2>We ship</h2>
+<h1>A FRIDGE MAGNET AND STICKERS</h1>
