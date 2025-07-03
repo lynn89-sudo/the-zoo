35c74de4d8901383958e9e6e59ff058a96455f9b
M J <luke87joseph@gmail.com>
2025-07-03 17:17:55 -0400

    Banner

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <div id="navbar">
-    <h1>placeholder</h1>
+    <img id="flag" src="flag.png" alt="Hand holding out a flag which says Hack Club, and links to the Hack Club Website"/>
+    <img id = "logo" src="logo.png" alt="Logo for The Zoo, a Hack Club You Ship We Ship program"/>
 </div>
+<div id="spacing"></div>
