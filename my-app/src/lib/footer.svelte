674914d5e7e97e68f1da1d7a2565fba02d1a31a1
M J <luke87joseph@gmail.com>
2025-07-21 00:06:15 -0400

    Added footer

---
+++ my-app/src/lib/footer.svelte
@@ -0,0 +1,27 @@
+<style>
+    div {
+        background-color: rgb(33, 39, 33);
+        font-family: Space Mono;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        button {
+            color: white;
+            font-family: Space Mono;
+            border: none;
+            margin-bottom: 10px;
+        }
+        button:hover {
+            background-color: darkgreen;
+        }
+        span {
+            margin-top: 10px;
+        }
+    }
+</style>
+<div>
+    <button onclick = { window.location.href = "https://hackclub.com" }><h4>Hack Club</h4></button>
+    <h5>The Zoo is built with love for teens, by teens</h5>
+    <span class="material-symbols-outlined">
+        pets
+    </span>
+</div>
