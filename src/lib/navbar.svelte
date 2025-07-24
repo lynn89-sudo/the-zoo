be9cb64fc760251c2d3afadb4293ea6f39cdf249
lynn89-sudo <luke87joseph@gmail.com>
2025-07-24 10:42:54 -0400

    Desktop Navbar

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -7,7 +7,7 @@
         height: 105px;
         z-index: 1000;
         background-color: rgba(40, 143, 40, 0.955);
-        transition: background-color 0.3s ease-in-out;
+        transition: background-color 0.3s ease-in-out, height 0.5s ease-in-out, border-bottom-right-radius 0.5s ease-in-out, border-bottom-left-radius 0.5s ease-in-out;
     }
 
     #logo {
@@ -16,11 +16,33 @@
         width: 250px;
         height: auto;
         margin-top: 10px;
+        user-select: none;
+        -webkit-user-select: none;
+        -moz-user-select: none;
         -webkit-user-drag: none;
         -moz-user-drag: none;
         -o-user-drag: none;
         cursor: pointer;
-        transition: opacity 0.3s ease-in-out; /* Transition for opacity */
+        transition: opacity 0.6s ease-in-out !important;
+    }
+
+    .navbutton {
+        margin-top: 105px;
+        margin-left: 5px;
+        margin-right: 5px;
+        border: none;
+        font-family: Montserrat, Space Mono;
+        font-weight: 700;
+        color: white;
+        background-color: rgb(2, 63, 2);
+        font-size: 20px;
+
+        opacity: 0;
+        visibility: collapse;
+        transition: opacity 0.5s ease-in-out;
+    }
+    .navbutton:hover {
+        background-color: rgb(7, 180, 7)
     }
 
     @media (max-width: 750px) {
@@ -38,30 +60,22 @@
             position: absolute;
             left: 0;
             right: 0;
-            transition: right 1s ease-in-out; /* Transition for position */
-        }
-
-        #navbar .navbutton {
-            margin-top: 30px;
-            background-color: darkgreen;
-            border: none;
-            opacity: 0;
-            visibility: hidden;
-            transition: opacity 1s ease-in-out;
-            margin-right: 20px;
+            transition: right 1s ease-in-out;
         }
     }
 
-    #navbar .navbutton {
-        opacity: 0;
-        visibility: hidden;
-        transition: opacity 1s ease-in-out;
-        margin-right: 20px;
-        color: white;
-    }
+    @media (min-width: 750px) {
+         #navbar:hover {
+            background-color: rgb(40, 143, 40);
+            height: 170px;
+            border-bottom-left-radius: 50px;
+            border-bottom-right-radius: 50px;
 
-    #navbar:hover {
-        background-color: rgb(40, 143, 40);
+            .navbutton {
+                visibility: visible;
+                opacity: 1;
+            }
+        }
     }
 
     #spacing {
@@ -77,6 +91,8 @@
 
 <div id="navbar">
     <a href= "/the-zoo/"><img id = "logo" src="logo.png" alt="Logo"/></a>
+    <button class="navbutton" onclick = { window.location.href = "/the-zoo/mynts" }>MYNTS</button>
+    <button class="navbutton" onclick = { window.location.href = "/the-zoo/requirements" }>REQUIREMENTS</button>
+    <button class="navbutton" onclick = { window.location.href = "/the-zoo/alternative" }>INTERNATIONAL SHIPPING</button>
 </div>
-<div id="linkto"></div>
 <div id="spacing"></div>
